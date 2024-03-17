91b1f1ed6ef4175b4e1f303a7fef689b6709ccab
Manoj Kelath <manojkelath1987@gmail.com>
2024-03-17 17:05:52 -0400

    added mock

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom, inject } from '@angular/core';
+import { LuxonDateAdapter } from '@angular/material-luxon-adapter';
+import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
+import { provideFuse } from '@fuse';
+// import { provideTransloco, TranslocoService } from '@ngneat/transloco';
+// import { firstValueFrom } from 'rxjs';
+import { routes } from './app.routes';
+import { provideAuth } from 'app/core/auth/auth.provider';
+import { provideIcons } from 'app/core/icons/icons.provider';
+import { mockApiServices } from 'app/mock-api';
+// import { TranslocoHttpLoader } from './core/transloco/transloco.http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
@@ -13,19 +21,88 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes), 
-    provideAnimationsAsync(),
-    provideHttpClient(),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-      })
-    ), provideAnimationsAsync(), provideAnimationsAsync(),
-  ],
-};
+    providers: [
+        provideAnimations(),
+        provideHttpClient(),
+        importProvidersFrom(
+          TranslateModule.forRoot({
+            loader: {
+              provide: TranslateLoader,
+              useFactory: HttpLoaderFactory,
+              deps: [HttpClient],
+            },
+          })),
+        provideRouter(routes,
+            withPreloading(PreloadAllModules),
+            withInMemoryScrolling({scrollPositionRestoration: 'enabled'}),
+        ),
 
+        // Material Date Adapter
+        {
+            provide : DateAdapter,
+            useClass: LuxonDateAdapter,
+        },
+        {
+            provide : MAT_DATE_FORMATS,
+            useValue: {
+                parse  : {
+                    dateInput: 'D',
+                },
+                display: {
+                    dateInput         : 'DDD',
+                    monthYearLabel    : 'LLL yyyy',
+                    dateA11yLabel     : 'DD',
+                    monthYearA11yLabel: 'LLLL yyyy',
+                },
+            },
+        },
+
+
+        // Fuse
+        provideAuth(),
+        provideIcons(),
+        provideFuse({
+            mockApi: {
+                delay   : 0,
+                services: mockApiServices,
+            },
+            fuse   : {
+                layout : 'classy',
+                scheme : 'light',
+                screens: {
+                    sm: '600px',
+                    md: '960px',
+                    lg: '1280px',
+                    xl: '1440px',
+                },
+                theme  : 'theme-default',
+                themes : [
+                    {
+                        id  : 'theme-default',
+                        name: 'Default',
+                    },
+                    {
+                        id  : 'theme-brand',
+                        name: 'Brand',
+                    },
+                    {
+                        id  : 'theme-teal',
+                        name: 'Teal',
+                    },
+                    {
+                        id  : 'theme-rose',
+                        name: 'Rose',
+                    },
+                    {
+                        id  : 'theme-purple',
+                        name: 'Purple',
+                    },
+                    {
+                        id  : 'theme-amber',
+                        name: 'Amber',
+                    },
+                ],
+            },
+        }),
+    ],
+};
